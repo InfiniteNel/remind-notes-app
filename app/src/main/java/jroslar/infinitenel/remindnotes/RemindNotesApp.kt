4336e901784b9d558967337f8dcb90eb9e3dc72b
InfiniteNel <juanrolagame@gmail.com>
2023-12-14 11:03:40 +0100

    Initial commit

---
+++ app/src/main/java/jroslar/infinitenel/remindnotes/RemindNotesApp.kt
@@ -0,0 +1,5 @@
+package jroslar.infinitenel.remindnotes
+
+import android.app.Application
+
+class RemindNotesApp : Application()
---
+++ app/src/main/java/jroslar/infinitenel/remindnotes/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package jroslar.infinitenel.remindnotes.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import jroslar.infinitenel.remindnotes.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
