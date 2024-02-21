0e033e94794d09c5a37d40b40c43083b835efd12
InfiniteNel <juanrolagame@gmail.com>
2024-02-21 10:57:57 +0100

    Añadido Menu Lateral.

--- app/src/main/java/jroslar/infinitenel/remindnotes/ui/MainActivity.kt
+++ app/src/main/java/jroslar/infinitenel/remindnotes/ui/MainActivity.kt
@@ -2,12 +2,36 @@ package jroslar.infinitenel.remindnotes.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import jroslar.infinitenel.remindnotes.R
+import jroslar.infinitenel.remindnotes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        initUI()
+    }
+
+    private fun initUI() {
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragment_container_iew) as NavHostFragment
+        navController = navHost.navController
+        binding.navView.setupWithNavController(navController)
     }
 }
