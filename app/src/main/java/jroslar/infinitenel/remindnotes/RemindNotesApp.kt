eeb975aaecac134a398220271018b18a28667d67
InfiniteNel <juanrolagame@gmail.com>
2024-02-23 18:38:04 +0100

    Añadido daggerHilt a ReminderNotesApp y MainActivity.

--- app/src/main/java/jroslar/infinitenel/remindnotes/RemindNotesApp.kt
+++ app/src/main/java/jroslar/infinitenel/remindnotes/RemindNotesApp.kt
@@ -1,5 +1,7 @@
 package jroslar.infinitenel.remindnotes
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class RemindNotesApp : Application()
--- app/src/main/java/jroslar/infinitenel/remindnotes/ui/MainActivity.kt
+++ app/src/main/java/jroslar/infinitenel/remindnotes/ui/MainActivity.kt
@@ -5,9 +5,11 @@ import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import dagger.hilt.android.AndroidEntryPoint
 import jroslar.infinitenel.remindnotes.R
 import jroslar.infinitenel.remindnotes.databinding.ActivityMainBinding
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
