57597fed5d232d9f6ce3dcb3345c5dc4b51aa146
InfiniteNel <juanrolagame@gmail.com>
2024-02-27 13:03:19 +0100

    Añadido editNoteMenu a EditNoteFragment.

--- app/src/main/java/jroslar/infinitenel/remindnotes/ui/managenote/EditNoteFragment.kt
+++ app/src/main/java/jroslar/infinitenel/remindnotes/ui/managenote/EditNoteFragment.kt
@@ -2,12 +2,20 @@ package jroslar.infinitenel.remindnotes.ui.managenote
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import dagger.hilt.android.AndroidEntryPoint
+import jroslar.infinitenel.remindnotes.R
 import jroslar.infinitenel.remindnotes.databinding.FragmentEditNoteBinding
 
-class EditNoteFragment : Fragment() {
+@AndroidEntryPoint
+class EditNoteFragment : Fragment(), MenuProvider {
 
     private var _binding: FragmentEditNoteBinding? = null
     private val binding get() = _binding!!
@@ -18,5 +26,31 @@ class EditNoteFragment : Fragment() {
     ): View {
         _binding = FragmentEditNoteBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initUI()
+    }
+
+    private fun initUI() {
+        initMenu()
+    }
+
+    private fun initMenu() {
+        requireActivity().addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.edit_note_menu, menu)
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        return when (menuItem.itemId) {
+            R.id.editNoteDelete -> true
+            R.id.editNoteSave -> true
+            else -> false
+        }
     }
 }
