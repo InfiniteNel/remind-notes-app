1f6a5c8b8b89171414146be1d10e732505bb2c7e
InfiniteNel <juanrolagame@gmail.com>
2024-03-01 16:14:05 +0100

    Añadido funcionalidad rellenar datos en EditNoteFragment.

--- app/src/main/java/jroslar/infinitenel/remindnotes/ui/managenote/EditNoteFragment.kt
+++ app/src/main/java/jroslar/infinitenel/remindnotes/ui/managenote/EditNoteFragment.kt
@@ -9,16 +9,24 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.navArgs
 import dagger.hilt.android.AndroidEntryPoint
 import jroslar.infinitenel.remindnotes.R
 import jroslar.infinitenel.remindnotes.databinding.FragmentEditNoteBinding
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class EditNoteFragment : Fragment(), MenuProvider {
 
     private var _binding: FragmentEditNoteBinding? = null
     private val binding get() = _binding!!
+
+    private val args: EditNoteFragmentArgs by navArgs()
+
+    private val viewmodel: EditNoteViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,6 +44,20 @@ class EditNoteFragment : Fragment(), MenuProvider {
 
     private fun initUI() {
         initMenu()
+        initData()
+    }
+
+    private fun initData() {
+        lifecycleScope.launch {
+            viewmodel.data.collect {note ->
+                binding.apply {
+                    tvEditNoteTitle.setText(note?.title)
+                    tvEditNoteDate.text = note?.noteDay
+                    tvEditNoteDescription.setText(note?.description)
+                }
+            }
+        }
+        viewmodel.getNote(args.noteId)
     }
 
     private fun initMenu() {
---
+++ app/src/main/java/jroslar/infinitenel/remindnotes/ui/managenote/EditNoteViewModel.kt
@@ -0,0 +1,29 @@
+package jroslar.infinitenel.remindnotes.ui.managenote
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import jroslar.infinitenel.remindnotes.domain.model.NoteModel
+import jroslar.infinitenel.remindnotes.domain.usecase.GetNoteByIdUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+@HiltViewModel
+class EditNoteViewModel @Inject constructor(
+    private val getNoteByIdUseCase: GetNoteByIdUseCase
+): ViewModel() {
+    private var _data = MutableStateFlow<NoteModel?>(null)
+    val data: StateFlow<NoteModel?> = _data
+
+    fun getNote(id: Int) {
+        viewModelScope.launch {
+            val result = withContext(Dispatchers.IO) { getNoteByIdUseCase(id) }
+
+            _data.value = result
+        }
+    }
+}
