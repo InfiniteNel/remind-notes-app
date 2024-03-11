b84c3effc2d59b3d902e553baf3197cce830c2ce
InfiniteNel <juanrolagame@gmail.com>
2024-03-11 18:19:03 +0100

    Añadido diseño pantalla RemindersFragment.

--- app/src/main/java/jroslar/infinitenel/remindnotes/ui/reminders/RemindersFragment.kt
+++ app/src/main/java/jroslar/infinitenel/remindnotes/ui/reminders/RemindersFragment.kt
@@ -5,9 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
 import jroslar.infinitenel.remindnotes.databinding.FragmentRemindersBinding
 
 
+@AndroidEntryPoint
 class RemindersFragment : Fragment() {
 
     private var _binding: FragmentRemindersBinding? = null
