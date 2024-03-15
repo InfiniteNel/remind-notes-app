ddfec65ff2b95959edef9d476ce7c2f0f4862a17
InfiniteNel <juanrolagame@gmail.com>
2024-03-15 13:06:47 +0100

    Añadido TimePickerDialog.

---
+++ app/src/main/java/jroslar/infinitenel/remindnotes/core/dialogs/TimePickerDialog.kt
@@ -0,0 +1,33 @@
+package jroslar.infinitenel.remindnotes.core.dialogs
+
+import android.app.Dialog
+import android.app.TimePickerDialog
+import android.content.Context
+import android.os.Bundle
+import android.widget.TimePicker
+import androidx.fragment.app.DialogFragment
+import java.util.Calendar
+
+class TimePickerDialog: DialogFragment(), TimePickerDialog.OnTimeSetListener {
+    private lateinit var onSelectTime: (time: String) -> Unit
+
+    companion object {
+        fun create(
+            listener: (time: String) -> Unit
+        ): jroslar.infinitenel.remindnotes.core.dialogs.TimePickerDialog = TimePickerDialog().apply {
+            onSelectTime = listener
+        }
+    }
+
+    override fun onTimeSet(view: TimePicker?, hour: Int, minute: Int) {
+        onSelectTime("%02d:%02d".format(hour, minute))
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val c = Calendar.getInstance()
+        val hour = c.get(Calendar.HOUR_OF_DAY)
+        val minute = c.get(Calendar.MINUTE)
+
+        return TimePickerDialog(activity as Context, this, hour, minute, true)
+    }
+}
