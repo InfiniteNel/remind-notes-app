5c0b5230adc9da160d6d72240699d205090ef490
InfiniteNel <juanrolagame@gmail.com>
2024-04-29 19:07:35 +0200

    Añadido actualizar notificación programada al actualizar Reminder.

--- app/src/main/java/jroslar/infinitenel/remindnotes/domain/usecase/UpdateReminderUseCase.kt
+++ app/src/main/java/jroslar/infinitenel/remindnotes/domain/usecase/UpdateReminderUseCase.kt
@@ -1,12 +1,26 @@
 package jroslar.infinitenel.remindnotes.domain.usecase
 
+import android.content.Context
+import dagger.hilt.android.qualifiers.ApplicationContext
+import jroslar.infinitenel.remindnotes.core.extensions.dateToMillisecondes
+import jroslar.infinitenel.remindnotes.core.extensions.timeToMillisecondes
+import jroslar.infinitenel.remindnotes.core.utils.NotificationUtils
 import jroslar.infinitenel.remindnotes.data.repository.ReminderRepositoryImpl
 import jroslar.infinitenel.remindnotes.domain.model.ReminderModel
 import javax.inject.Inject
 
 class UpdateReminderUseCase @Inject constructor(
-    private val repositoryImpl: ReminderRepositoryImpl
+    private val repositoryImpl: ReminderRepositoryImpl,
+    @ApplicationContext private val context: Context
 ) {
-    suspend operator fun invoke(reminderModel: ReminderModel) =
+    suspend operator fun invoke(reminderModel: ReminderModel) {
         repositoryImpl.updateReminder(reminderModel)
+        NotificationUtils.createScheduledNotification(
+            context,
+            reminderModel.remindDay.dateToMillisecondes() + reminderModel.timeDay.timeToMillisecondes(),
+            reminderModel.id,
+            reminderModel.title,
+            reminderModel.description
+        )
+    }
 }
