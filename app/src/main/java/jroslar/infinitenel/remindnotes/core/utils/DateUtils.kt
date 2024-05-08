3811ff61e25ffca8ab9d0181d9d11671456cb6f5
InfiniteNel <juanrolagame@gmail.com>
2024-05-08 17:22:31 +0200

    Creado función getNextDateOfDayOfWeek en DateUtils.

--- app/src/main/java/jroslar/infinitenel/remindnotes/core/utils/DateUtils.kt
+++ app/src/main/java/jroslar/infinitenel/remindnotes/core/utils/DateUtils.kt
@@ -1,6 +1,7 @@
 package jroslar.infinitenel.remindnotes.core.utils
 
 import android.os.Build
+import androidx.annotation.RequiresApi
 import jroslar.infinitenel.remindnotes.core.Constant
 import java.text.SimpleDateFormat
 import java.time.LocalDate
@@ -20,5 +21,17 @@ object DateUtils {
             val formatter = SimpleDateFormat(Constant.FORMAT_DATE_OVER_API_26, Locale.getDefault())
             formatter.format(inputDate!!)
         }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun getNextDateOfDayOfWeek(dayOfWeek: DaysOfWeek): Long {
+        val today = LocalDate.now()
+        val currentDayOfWeek = today.dayOfWeek
+
+        val daysToAdd = (dayOfWeek.ordinal - currentDayOfWeek.value + 7) % 7 + 1
+
+        val nextDate = today.plusDays(daysToAdd.toLong())
+
+        return nextDate.toEpochDay() * 24 * 60 * 60 * 1000
     }
 }
